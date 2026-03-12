4df796eb6bc107bddf229f0811484763526e4674
Lucas Marcolongo <lucas@marcolongo.dev>
2026-03-12 11:18:32 -0700

    feat: add debugprint plugin with debug keymaps under <leader>d

--- plugins/common/plugins.nix
+++ plugins/common/plugins.nix
@@ -3,11 +3,29 @@
   config,
   ...
 }: let
+  inherit (config.nixievim.mkKey) wKeyObj;
 in {
   extraPlugins = with pkgs.vimPlugins; [stay-centered-nvim];
   plugins = {
     # Must have plugins to have a decent flow of work
     comment.enable = true;
+    debugprint = {
+      enable = true;
+      settings.keymaps = {
+        normal = {
+          plain_below = "<leader>dp";
+          plain_above = "<leader>dP";
+          variable_below = "<leader>dv";
+          variable_above = "<leader>dV";
+          delete_debug_prints = "<leader>dD";
+          toggle_comment_debug_prints = "<leader>dt";
+        };
+        visual = {
+          variable_below = "<leader>dv";
+          variable_above = "<leader>dV";
+        };
+      };
+    };
     web-devicons.enable = true;
     nvim-surround.enable = true;
     nvim-autopairs.enable = true;
@@ -29,6 +47,11 @@ in {
       settings.preset = "modern";
     };
   };
+
+  wKeyList = [
+    (wKeyObj ["<leader>d" "󰃤" "debug"])
+  ];
+
   opts = {
     timeout = true;
     timeoutlen = 250;
